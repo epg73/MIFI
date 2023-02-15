0c7aa5413f1ba8101a9d1cd5670f5c8b482900d8
epg <epg73@mail.ru>
2023-02-15 12:59:31 +0300

    HW6 version 1

--- HW6/main.py
+++ HW6/main.py
@@ -8,7 +8,7 @@ def do_ping_sweep(site):
         print(line.encode('cp1251').decode('cp866'))
 
 
-list_of_sites = ['ya.ru', 'google.com', 'skillfactory.ru']
+list_of_sites = ['newsler.ru', 'google.com', 'skillfactory.ru']
 
 for site in list_of_sites:
     do_ping_sweep(site)
